d9a74e30ae11de940195803f35574335e1b72c92
Mevan Samaratunga <mevan.samaratunga@fmr.com>
2014-08-07 09:03:37 -0400

    remove test data

--- test_data/knife_upload_databag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Y | knife data bag delete users osenvtest
-knife data bag create users
-knife data bag from file users ./user_data_bag.json --secret 1234
-
-echo Y | knife data bag delete service_endpoints qip
-knife data bag create service_endpoints
-knife data bag from file service_endpoints ./qip_data_bag.json --secret 1234
