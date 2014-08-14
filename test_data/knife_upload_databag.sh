027257443f3b062d73199b4d02c56cacdd65fbf9
Mevan Samaratunga <mevan.samaratunga@fmr.com>
2014-08-14 14:52:16 -0400

    changed how env key is used when looking up a databag

--- test_data/knife_upload_databag.sh
+++ test_data/knife_upload_databag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo Y | knife data bag delete users "test._default"
-knife data bag create users
-knife data bag from file users ./data-bag_user.json --secret 1234
+echo Y | knife data bag delete "users-_default" "test"
+knife data bag create "users-_default"
+knife data bag from file "users-_default" ./data-bag_user.json --secret 1234
