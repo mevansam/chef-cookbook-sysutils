8dcd66bb6cb8449d9b1b212f363861c5e4988164
Mevan Samaratunga <mevan.samaratunga@fmr.com>
2014-09-10 22:01:59 -0400

    updated

--- recipes/cluster.rb
+++ recipes/cluster.rb
@@ -4,6 +4,9 @@
 #
 # Copyright (c) 2014 Fidelity Investments.
 #
+# Author: Mevan Samaratunga
+# Email: mevan.samaratunga@fmr.com
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
